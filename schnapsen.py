a3e6aa7b63a8350056173e90a00e70529bfa9c6b
Thomas Mitzka <tm@thomasmitzka.at>
2018-05-22 19:09:25 +0200

    first commit

---
+++ schnapsen.py
@@ -0,0 +1,196 @@
+"""A game of Schnapsen.
+
+https://en.wikipedia.org/wiki/Sixty-six_(card_game)#Schnapsen
+"""
+
+from time import sleep
+
+from schnapsen_classes import SchnapsenPlayer, SchnapsenGame
+
+
+PLAYER_NAMES = ("Human", "Computer")
+
+# Change this number to make the game run slower or faster.
+SECONDS = 1.5
+
+
+def create_players():
+    """Create the players."""
+    player1 = SchnapsenPlayer(PLAYER_NAMES[0], human=True)
+    player2 = SchnapsenPlayer(PLAYER_NAMES[1])
+    players = [player1, player2]
+    print(f"Welcome, {player1.name} and {player2.name}!", end=" ")
+    print(f"{player1.name}, you will begin.")
+    return players
+
+
+def start_new_game(players):
+    """Create the game instance. Shuffle and deal the cards."""
+    game = SchnapsenGame()
+    game.create_cards()
+    game.shuffle_cards()
+    for player in players:
+        for dummy in range(5):
+            card = game.draw_from_stock()
+            player.add_card(card)
+    return game
+
+
+def game_loop(game, players):
+    """The game loop."""
+    while True:
+
+        # Start a new trick.
+        trick = []
+        for player in players:
+
+            # The players have played all their cards
+            # before one of them could achieve 66 points.
+            if not player.cards:
+                print("\nWe've run out of cards.")
+                print("The player who took the last trick wins.")
+                return
+
+            # Players may exchange the trump Jack or close the stock."""
+            if not trick and not game.closed:
+                if player.human:
+                    exchange, close = player.choose_action_human(
+                        game.trump_suit, game.trump_card
+                    )
+                else:
+                    exchange, close = player.choose_action_computer(
+                        game.trump_suit
+                    )
+
+                # A player has chosen to exchange the trump Jack
+                # for the trump card.
+                if exchange and not game.closed:
+                    trump_jack = player.pop_trump_jack(game.trump_suit)
+                    if player.human:
+                        print(f"\n< You take the {game.trump_card['name']}.")
+                    else:
+                        print(f"\n< {player.name} takes the", end=" ")
+                        print(f"{game.trump_card['name']}.")
+                    game.exchange_trump_jack(trump_jack)
+
+                # A player has chosen to close the stock.
+                if close:
+                    game.close_stock()
+
+            sleep(SECONDS)
+            # Player chooses a card to play.
+            couples = player.get_couples()
+
+            if player.human:
+                chosen_card = player.choose_card_human(
+                    couples, game.trump_suit, trick, game.closed
+                )
+            elif not player.human and not trick:
+                chosen_card = player.choose_card1_computer(
+                    couples, game.trump_suit
+                )
+            else:
+                chosen_card = player.choose_card2_computer(
+                    game.trump_suit, trick, game.closed
+                )
+
+            # Check for a victory by marriage.
+            if player.points >= 66:
+                return
+
+            # Player has chosen a card to play.
+            card_tuple = (chosen_card, player.name)
+            trick.append(card_tuple)
+            if player.human:
+                print(f"> You play the {chosen_card['name']}.")
+            else:
+                print(f"> {player.name} plays the", end=" ")
+                print(f"{chosen_card['name']}.")
+
+        sleep(SECONDS)
+
+        # One of the players takes the trick.
+        if chosen_card:
+            taker = game.decide_taker(trick, game.trump_suit)
+            for player in players:
+                if player.name == taker:
+                    if player.human:
+                        print("\nYou take this trick.")
+                    else:
+                        print(f"\n{player.name} takes this trick.")
+                    player.add_trick_points(trick)
+
+            # If the second player has taken the trick, switch turns.
+            if taker == players[1].name:
+                players.reverse()
+
+            # Check for a victory by trick-taking.
+            for player in players:
+                if player.points >= 66:
+                    return
+
+            # Both players draw cards.
+            sleep(SECONDS)
+            for player in players:
+                sleep(SECONDS)
+                card = game.draw_from_stock()
+                if card:
+                    player.add_card(card)
+
+
+def results(players):
+    """Declare the winner, raise score, and show results."""
+    if players[0].human:
+        print("You WIN this round!")
+    else:
+        print(f"{players[0].name} WINS this round!")
+    input("Press Enter to see the results. ")
+
+    if players[1].points == 0:
+        players[0].score += 3
+    elif players[1].points < 33:
+        players[0].score += 2
+    else:
+        players[0].score += 1
+
+    print("\nPOINTS")
+    player_points = sorted(players, key=lambda p: p.points, reverse=True)
+    for player in player_points:
+        print(f"{player.name}: {player.points}")
+
+    print("\nTOTAL SCORE")
+    player_score = sorted(players, key=lambda p: p.score, reverse=True)
+    for player in player_score:
+        print(f"{player.name}: {player.score}")
+
+
+def reset_attributes(players):
+    """Reset players' attributes to prepare for the next round."""
+    for player in players:
+        player.points, player.marriage_points = 0, 0
+        player.cards = []
+        player.trump_jack = False
+    return players
+
+
+def main():
+    """The main function."""
+    print("SCHNAPSEN\n")
+    players = create_players()
+    while True:
+        game = start_new_game(players)
+        game_loop(game, players)
+        results(players)
+
+        answer = ""
+        while answer not in ("y", "n"):
+            answer = input("\nStart a new game? (y/n) ").strip().lower()
+        if answer == "y":
+            players = reset_attributes(players)
+            print("Both players will keep their current score.")
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
---
+++ schnapsen_classes.py
@@ -0,0 +1,355 @@
+"""Classes for a game of Schnapsen."""
+
+import random
+from collections import defaultdict
+
+SUITS = ("Clubs", "Diamonds", "Hearts", "Spades")
+
+
+class SchnapsenPlayer():
+    """A class to represent a player of Schnapsen."""
+
+    def __init__(self, name, human=False):
+        """Initialize attributes."""
+        self.name = name
+        self.human = human
+        self.cards = []
+        self.points, self.marriage_points = 0, 0
+        self.score = 0
+
+    def __repr__(self):
+        return f"{self.name} is a player of Schnapsen."
+
+    def add_card(self, card):
+        """Add a card for the player's hand."""
+        self.cards.append(card)
+        if self.human:
+            print(f"You have a new card: {card['name']}")
+        else:
+            print(f"{self.name} has a new card.")
+
+    def get_couples(self):
+        """Return a dictionary of matching kings and queens."""
+        kings_queens = defaultdict(list)
+        for card in self.cards:
+            if card["rank"] in ("King", "Queen"):
+                suit = card["suit"]
+                kings_queens[suit].append(card)
+        return {k: v for (k, v) in kings_queens.items() if len(v) == 2}
+
+    def choose_card_human(self, couples, trump_suit, trick, closed):
+        """Let human player choose a card, and return it."""
+        # Sort cards to show them in order.
+        self.cards.sort(
+            key=lambda card: (card["suit"], card["points"]), reverse=True
+        )
+
+        # Show enumerated cards.
+        print()
+        for counter, card in enumerate(self.cards, 1):
+            if card["suit"] == trump_suit:
+                print(f"{counter} - {card['name']}*")
+            else:
+                print(f"{counter} - {card['name']}")
+        # Show the "marry" option.
+        if couples and not trick:
+            print("M - Marry a couple")
+
+        while True:
+            print()
+            if trick and closed:
+                print("You have to match suit and take the trick if you can.")
+            user_input = input("Your choice: ").strip()
+            if (
+                    user_input.isdigit() and
+                    int(user_input) in range(1, len(self.cards)+1)
+            ):
+                index = int(user_input) - 1
+                chosen_card = self.cards[index]
+                break
+
+            # Player chooses a card from the matching couples to play.
+            elif user_input.upper() == "M" and couples and not trick:
+                couple_cards = []
+                for suit in couples:
+                    couple_cards.extend(couples[suit])
+
+                print()
+                for counter, card in enumerate(couple_cards, 1):
+                    if card["suit"] == trump_suit:
+                        print(f"{counter} - {card['name']}*")
+                    else:
+                        print(f"{counter} - {card['name']}")
+                user_input = ""
+                while (
+                        not user_input.isdigit() or
+                        int(user_input) not in range(1, len(couple_cards)+1)
+                ):
+                    user_input = input("\nChoose a card: ").strip()
+                index = int(user_input) - 1
+                chosen_card = couple_cards[index]
+                self.marry(chosen_card, trump_suit)
+                break
+
+            else:
+                print("Please choose one of the options above.")
+
+        self.cards.remove(chosen_card)
+        return chosen_card
+
+    def choose_card1_computer(self, couples, trump_suit):
+        """Choose the computer's first card in this trick."""
+        if couples:
+            # If you have the trump couple, marry it. Otherwise,
+            # marry the couple of a random suit in the list.
+            if trump_suit in couples:
+                chosen_card = random.choice(couples[trump_suit])
+                self.marry(chosen_card, trump_suit)
+            else:
+                suit = random.choice(list(couples))
+                chosen_card = random.choice(couples[suit])
+                self.marry(chosen_card, trump_suit)
+        # If you have no couples, play your first card.
+        else:
+            chosen_card = self.cards[0]
+
+        self.cards.remove(chosen_card)
+        return chosen_card
+
+    def choose_card2_computer(self, trump_suit, trick, closed):
+        """Choose the computer's second card in this trick."""
+        opponent_card = trick[0][0]
+
+        higher_suit_cards, lower_suit_cards = [], []
+        trump_cards = []
+
+        for card in self.cards:
+            if (
+                    card["suit"] == opponent_card["suit"] and
+                    card["points"] > opponent_card["points"]
+                ):
+                higher_suit_cards.append(card)
+            elif (
+                    card["suit"] == opponent_card["suit"] and
+                    card["points"] < opponent_card["points"]
+                ):
+                lower_suit_cards.append(card)
+            elif (
+                    card["suit"] == trump_suit and
+                    opponent_card["suit"] != trump_suit
+                ):
+                trump_cards.append(card)
+
+        # Never choose a card with a higher value than required.
+        if higher_suit_cards:
+            higher_suit_cards.sort(key=lambda d: d["points"])
+            chosen_card = higher_suit_cards[0]
+        # If the stock is closed, you have to match the suit.
+        elif closed and lower_suit_cards:
+            lower_suit_cards.sort(key=lambda d: d["points"])
+            chosen_card = lower_suit_cards[0]
+        elif trump_cards:
+            trump_cards.sort(key=lambda d: d["points"])
+            chosen_card = trump_cards[0]
+        # If none of the above is true, choose your lowest card,
+        # but not a trump card.
+        else:
+            no_trumps = [c for c in self.cards if c["suit"] != trump_suit]
+            no_trumps.sort(key=lambda d: d["points"])
+            chosen_card = no_trumps[0]
+
+        self.cards.remove(chosen_card)
+        return chosen_card
+
+    def add_trick_points(self, trick):
+        """Count and add the points of a taken trick."""
+        for card_tuple in trick:
+            card = card_tuple[0]
+            self.points += card["points"]
+        if self.marriage_points:
+            self.points += self.marriage_points
+            self.marriage_points = 0
+            print("Marriage points added.")
+        if self.human:
+            print("Your", end=" ")
+        else:
+            print(f"{self.name}'s", end=" ")
+        print(f"points: {self.points} / 66")
+
+    def choose_action_human(self, trump_suit, trump_card):
+        """Human player may choose an action.
+
+        The player can exchange the trump Jack and/or close the stock.
+        """
+        exchange, close = False, False
+        choices = ["Close the stock"]
+        for card in self.cards:
+            if card["rank"] == "Jack" and card["suit"] == trump_suit:
+                exchange_choice = f"Exchange your {card['name']} " \
+                f"for the {trump_card['name']}"
+                choices.append(exchange_choice)
+                break
+
+        while True:
+            if choices:
+                print("\nYou will play the first card.", end=" ")
+                print("Do you want to perform an action?\n")
+                for counter, choice in enumerate(choices, 1):
+                    print(f"{counter} - {choice}")
+                prompt = "\nChoose an action or press Enter: "
+                user_input = input(prompt).strip()
+            else:
+                break
+            if user_input:
+                if user_input == "1":
+                    close = True
+                    break
+                elif user_input == "2" and len(choices) > 1:
+                    exchange = True
+                    choices.pop(1)
+                else:
+                    print("Please choose one of the options above", end=" ")
+                    print("or press Enter.")
+            else:
+                break
+
+        return exchange, close
+
+    def choose_action_computer(self, trump_suit):
+        """Computer player will exchange the trump card, if possible."""
+        exchange, close = False, False
+        for card in self.cards:
+            if card["rank"] == "Jack" and card["suit"] == trump_suit:
+                exchange = True
+                break
+        return exchange, close
+
+    def marry(self, card, trump_suit):
+        """Marry a king and a queen."""
+        if card["suit"] == trump_suit:
+            points = 40
+        else:
+            points = 20
+
+        # Marriage points are only counted after the player has taken
+        # at least one trick.
+        if self.human:
+            print("X You marry the couple of", end=" ")
+        else:
+            print(f"X {self.name} marries the couple of", end=" ")
+        print(f"{card['suit']} ({points} points).")
+
+        if self.points:
+            self.points += points
+        else:
+            self.marriage_points += points
+            print("Points will be added after the first taken trick.")
+
+    def pop_trump_jack(self, trump_suit):
+        """Remove and return the trump Jack from the player's cards."""
+        for card in self.cards:
+            if card["rank"] == "Jack" and card["suit"] == trump_suit:
+                trump_jack = card
+                break
+        self.cards.remove(trump_jack)
+        return trump_jack
+
+
+class SchnapsenGame():
+    """A class to represent a game of Schnapsen."""
+
+    def __init__(self):
+        """Initialize attributes."""
+        self.stock = []
+        self.trump_card, self.trump_suit = None, None
+        self.closed = False
+
+    def __repr__(self):
+        return "Schnapsen is a text-based card game for two players."
+
+    def create_cards(self):
+        """Create the cards."""
+        ranks_points = (
+            ("Jack", 2),
+            ("Queen", 3),
+            ("King", 4),
+            ("Ten", 10),
+            ("Ace", 11)
+        )
+
+        for suit in SUITS:
+            for rank, points in ranks_points:
+                name = f"[{rank} of {suit}]"
+                card = {
+                    "name": name,
+                    "suit": suit,
+                    "rank": rank,
+                    "points": points
+                }
+                self.stock.append(card)
+
+    def shuffle_cards(self):
+        """Shuffle the cards."""
+        random.shuffle(self.stock)
+        self.trump_card = self.stock[0]
+        self.trump_suit = self.trump_card["suit"]
+        print("Cards are shuffled.", end=" ")
+        print(f"Trump card: {self.trump_card['name']}\n")
+
+    @staticmethod
+    def decide_taker(trick, trump_suit):
+        """Decide who takes the trick."""
+        (card1, player1), (card2, player2) = trick
+        if card1["suit"] == card2["suit"]:
+            # Both cards have the same suit.
+            # The player with the higher card takes the trick.
+            trick.sort(key=lambda t: t[0]["points"])
+            card_tuple = trick[-1]
+            taker = card_tuple[1]
+        else:
+            # Cards have different suites.
+            # If player2 chose a trump suit card, he takes the trick.
+            if card2["suit"] == trump_suit:
+                taker = player2
+            else:
+                taker = player1
+        return taker
+
+    def draw_from_stock(self):
+        """Draw a new card from the stock."""
+        if not self.closed:
+            card = self.stock.pop()
+            if len(self.stock) == 2:
+                print("\nThere are only two more cards in the stock.\n")
+            elif not self.stock:
+                print(
+                    "\nThe last card has been drawn!\n"
+                    "From now on, players must match suits "
+                    "and take tricks if they can.\n"
+                )
+                self.closed = True
+            return card
+        else:
+            return None
+
+    def close_stock(self):
+        """Close the stock."""
+        self.closed = True
+        print(
+            "The stock is closed. "
+            "Players cannot draw cards anymore."
+        )
+        print(
+            "From now on, players must match suits "
+            "and take tricks if they can."
+        )
+
+    def exchange_trump_jack(self, trump_jack):
+        """Exchange a trump Jack for the trump card."""
+        self.stock[0] = trump_jack
+        self.trump_card = trump_jack
+        print(f"New trump card: {self.trump_card['name']}")
+
+
+if __name__ == "__main__":
+    print("This module is meant to be imported by the main module.")
